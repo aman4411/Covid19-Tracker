8fdb2849b53337a7ca33e61c1cf7b1c14b6e6d42
aman4411 <amanmehta4411@gmail.com>
2020-05-06 19:13:52 +0530

    fetching data from api

--- app/src/main/java/com/amanmehta/covid19_tracker/MainActivity.java
+++ app/src/main/java/com/amanmehta/covid19_tracker/MainActivity.java
@@ -2,13 +2,105 @@ package com.amanmehta.covid19_tracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ScrollView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.leo.simplearcloader.SimpleArcLoader;
+
+import org.eazegraph.lib.charts.PieChart;
+import org.eazegraph.lib.models.PieModel;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.StringReader;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvCases,tvRecovered,tvCritical,tvActive,tvTodayCases,tvTotalDeaths,tvTodayDeaths,tvCountriesAffected;
+    ScrollView scrollStats;
+    SimpleArcLoader simpleArcLoader;
+    PieChart pieChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvCases = findViewById(R.id.tvCases);
+        tvRecovered = findViewById(R.id.tvRecovered);
+        tvCritical = findViewById(R.id.tvCritical);
+        tvActive = findViewById(R.id.tvActive);
+        tvTodayCases = findViewById(R.id.tvTodayCases);
+        tvTotalDeaths = findViewById(R.id.tvTotalDeaths);
+        tvTodayDeaths = findViewById(R.id.tvTodayDeaths);
+        tvCountriesAffected = findViewById(R.id.tvCountriesAffected);
+
+        scrollStats = findViewById(R.id.scrollStats);
+        pieChart = findViewById(R.id.piechart);
+        simpleArcLoader = findViewById(R.id.loader);
+        
+        fetchData();
+    }
+
+    private void fetchData() {
+
+        String url = "https://corona.lmao.ninja/v2/all/";
+
+        simpleArcLoader.start();
+        StringRequest request = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject jsonObject = new JSONObject(response.toString());
+
+//                    Fetching Data from API
+                    tvCases.setText(jsonObject.getString("cases"));
+                    tvRecovered.setText(jsonObject.getString("recovered"));
+                    tvCritical.setText(jsonObject.getString("critical"));
+                    tvActive.setText(jsonObject.getString("active"));
+                    tvTodayCases.setText(jsonObject.getString("todayCases"));
+                    tvTotalDeaths.setText(jsonObject.getString("deaths"));
+                    tvTodayDeaths.setText(jsonObject.getString("todayDeaths"));
+                    tvCountriesAffected.setText(jsonObject.getString("affectedCountries"));
+
+//                    Setting Data in Pie Chart
+                    pieChart.addPieSlice(new PieModel("Cases",Integer.parseInt(tvCases.getText().toString()), Color.parseColor("#ffa726")));
+                    pieChart.addPieSlice(new PieModel("Recovered",Integer.parseInt(tvRecovered.getText().toString()), Color.parseColor("#66bb6a")));
+                    pieChart.addPieSlice(new PieModel("Total Deaths",Integer.parseInt(tvTotalDeaths.getText().toString()), Color.parseColor("#ef5350")));
+                    pieChart.addPieSlice(new PieModel("Active",Integer.parseInt(tvActive.getText().toString()), Color.parseColor("#29b6f6")));
+                    pieChart.startAnimation();
+                    simpleArcLoader.stop();
+                    simpleArcLoader.setVisibility(View.GONE);
+                    scrollStats.setVisibility(View.VISIBLE);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    simpleArcLoader.stop();
+                    simpleArcLoader.setVisibility(View.GONE);
+                    scrollStats.setVisibility(View.VISIBLE);
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                simpleArcLoader.stop();
+                simpleArcLoader.setVisibility(View.GONE);
+                scrollStats.setVisibility(View.VISIBLE);
+                Toast.makeText(getApplicationContext(),error.getMessage(),Toast.LENGTH_LONG).show();
+            }
+        });
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(request);
     }
 }
